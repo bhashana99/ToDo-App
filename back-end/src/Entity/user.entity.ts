deb8e9deb4d596009b712c4385dced9107384727
bhashana99 <bhashanachamodya99@gmail.com>
2025-05-24 22:30:55 +0530

    Define Entity for Users

---
+++ back-end/src/Entity/user.entity.ts
@@ -0,0 +1,18 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity('users')
+export class UserEntity{
+
+    @PrimaryGeneratedColumn()
+    id:number;
+
+    @Column()
+    username: string;
+
+    @Column()
+    password: string;
+
+    @Column()
+    salt: string;
+
+}
