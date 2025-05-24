70db70892b2d7701018d8b1511b2adf94ba91ea6
bhashana99 <bhashanachamodya99@gmail.com>
2025-05-24 22:22:19 +0530

    generate new controller, service and module for auth

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TodoModule } from './todo/todo.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -17,6 +18,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
     }),
+    AuthModule,
 
   ],
   controllers: [AppController],
