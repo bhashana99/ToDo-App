9282ffb4b53a48a1dd890781d4e4b0931ec923ee
bhashana99 <bhashanachamodya99@gmail.com>
2025-05-24 15:58:59 +0530

    fetch all todos from DB - change update getAllToDos method

--- back-end/src/todo/todo.service.ts
+++ back-end/src/todo/todo.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ToDoEntity } from 'src/Entity/todo.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class TodoService {
 
-    getAllToDos(){
-        return ["q1","q2"];
+    constructor(@InjectRepository (ToDoEntity) private todoRepo:Repository<ToDoEntity>){}
+
+    async getAllToDos(){
+        return await this.todoRepo.find();
     }
 }
