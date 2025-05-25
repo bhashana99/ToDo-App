1ff595feffc2239f366648fd78f36ecd7eb77986
bhashana99 <bhashanachamodya99@gmail.com>
2025-05-25 08:51:48 +0530

    install and import required packeges for user login auth

--- back-end/src/auth/auth.module.ts
+++ back-end/src/auth/auth.module.ts
@@ -1,9 +1,11 @@
+import { Jwt } from './../../node_modules/@types/jsonwebtoken/index.d';
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from 'src/Entity/user.entity';
 import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
   imports:[
@@ -14,6 +16,9 @@ import { JwtModule } from '@nestjs/jwt';
         algorithm: 'HS512',
         expiresIn:'1d'
       }
+    }),
+    PassportModule.register({
+      defaultStrategy:'Jwt'
     })
   ],
   providers: [AuthService],
