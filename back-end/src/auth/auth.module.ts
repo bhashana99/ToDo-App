6671fb6ac008775e00cfa3aa7bddfa4f8be49fe5
bhashana99 <bhashanachamodya99@gmail.com>
2025-05-25 07:51:13 +0530

    install JWT

--- back-end/src/auth/auth.module.ts
+++ back-end/src/auth/auth.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from 'src/Entity/user.entity';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
+  imports:[
+    TypeOrmModule.forFeature([UserEntity]),
+    JwtModule.register({
+      secret: "secretKeyThisShouldPrivateInDotENVfile",
+      signOptions:{
+        algorithm: 'HS512',
+        expiresIn:'1d'
+      }
+    })
+  ],
   providers: [AuthService],
   controllers: [AuthController]
 })
--- back-end/src/auth/auth.service.ts
+++ back-end/src/auth/auth.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserEntity } from 'src/Entity/user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+
+    constructor(@InjectRepository(UserEntity) private repo:Repository<UserEntity>){}
+
+}
