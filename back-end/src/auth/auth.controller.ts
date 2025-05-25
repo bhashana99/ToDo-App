0120401465b81db34f381ad3315172f6ce154c47
bhashana99 <bhashanachamodya99@gmail.com>
2025-05-25 08:34:31 +0530

    user Registration end-point & methods

--- back-end/src/auth/auth.controller.ts
+++ back-end/src/auth/auth.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { UserRegistrationDTO } from 'src/DTO/UserRegistration.dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+
+    constructor(private authService:AuthService){}
+
+    @Post('register')
+    UserRegistration(@Body(ValidationPipe) registrationDto:UserRegistrationDTO){
+       return this.authService.userRegistration(registrationDto);
+    }
+}
--- back-end/src/auth/auth.service.ts
+++ back-end/src/auth/auth.service.ts
@@ -1,11 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { UserRegistrationDTO } from 'src/DTO/UserRegistration.dto';
 import { UserEntity } from 'src/Entity/user.entity';
 import { Repository } from 'typeorm';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class AuthService {
 
     constructor(@InjectRepository(UserEntity) private repo:Repository<UserEntity>){}
 
+
+    async userRegistration(userRegistrationDto:UserRegistrationDTO){
+        const {username,password} = userRegistrationDto;
+
+        const hashed = await bcrypt.hash(password,10);
+        const salt =  bcrypt.getSalt(hashed);
+
+        const newUser = new UserEntity();
+
+        newUser.username = username;
+        newUser.password = hashed;
+        newUser.salt = salt;
+
+        try {
+            return this.repo.save(newUser);
+        } catch (error) {
+             throw new InternalServerErrorException("Something went wrong, user not created.") 
+        }
+        
+    }
 }
