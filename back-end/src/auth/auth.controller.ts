b8516d7f83cff6737f34da199540185cc2cffbef
bhashana99 <bhashanachamodya99@gmail.com>
2025-05-25 09:36:03 +0530

    generate JWt Token for valid credentials

--- back-end/src/auth/auth.controller.ts
+++ back-end/src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Get, Post, ValidationPipe } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { UserRegistrationDTO } from 'src/DTO/UserRegistration.dto';
+import { UserLogInDTO } from 'src/DTO/UserLogin.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -10,5 +11,10 @@ export class AuthController {
     @Post('register')
     UserRegistration(@Body(ValidationPipe) registrationDto:UserRegistrationDTO){
        return this.authService.userRegistration(registrationDto);
+    }
+
+    @Post('login')
+    UserLogin(@Body(ValidationPipe) userLoginDto:UserLogInDTO){
+        return this.authService.userLogin(userLoginDto);
     }
 }
--- back-end/src/auth/auth.service.ts
+++ back-end/src/auth/auth.service.ts
@@ -1,14 +1,17 @@
-import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { Injectable, InternalServerErrorException, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserRegistrationDTO } from 'src/DTO/UserRegistration.dto';
 import { UserEntity } from 'src/Entity/user.entity';
 import { Repository } from 'typeorm';
 import * as bcrypt from 'bcryptjs';
+import { UserLogInDTO } from 'src/DTO/UserLogin.dto';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
 
-    constructor(@InjectRepository(UserEntity) private repo:Repository<UserEntity>){}
+    constructor(@InjectRepository(UserEntity) private repo:Repository<UserEntity>, 
+                                              private jwt:JwtService){}
 
 
     async userRegistration(userRegistrationDto:UserRegistrationDTO){
@@ -27,7 +30,30 @@ export class AuthService {
             return this.repo.save(newUser);
         } catch (error) {
              throw new InternalServerErrorException("Something went wrong, user not created.") 
+        }  
+    }
+
+    async userLogin(userLoginDto:UserLogInDTO){
+        const {username,password} = userLoginDto;
+
+        const user = await this.repo.findOne({where:{username}});
+
+        if(!user){
+            throw new UnauthorizedException("Invalid credentials");
         }
-        
+
+        const isPasswordMatch = await bcrypt.compare(password,user.password);
+
+        if(isPasswordMatch){
+            const jwtPayload = {username};
+            const jwtToken = await this.jwt.signAsync(jwtPayload)
+
+            return {token:jwtToken}
+
+        }else{
+            throw new UnauthorizedException('Invalid credentials')
+        }
+
+
     }
 }
